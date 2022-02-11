a7622df2630494ab7edbac80ff59142aa35e3934
Chinjan Patel <patel.chinjan531@gmail.com>
2022-02-11 13:00:39 +0530

    Update to Ciorra Diamonds

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'charmiindia' );
+define('DB_NAME', 'onpszdmy_WPCHARMI');
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define('DB_USER', 'onpszdmy_WPMWP');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define('DB_PASSWORD', 'iY]OM1^4uILW3V^]w');
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
